b68dabfdd33b3a09ea893d56feab02ca1410e785
JUAN DIEGO SOTO CASTRO <juan.sotocastro@ucr.ac.cr>
2024-03-17 22:47:18 -0600

    statistics print help working

--- taller_c/src/main.c
+++ taller_c/src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <statistics.h>
 
 int main(int argc, char *argv[])
 {
+  int error = EXIT_SUCCESS;
+
   statistics_t* statistics = statistics_create();
 
-  int error = statistics_run(statistics, argc, argv);
-  statistics_destroy(statistics);
+  // Codigo similar a un try catch en el main
+  if (statistics) {
+  
+    error = statistics_run(statistics, argc, argv);
+    statistics_destroy(statistics);
+   
+  } else{
+    fprintf(stderr, "error: could not allocate statistics object \n");
+  }
 
   return error;
 }
--- taller_c/src/statistics.c
+++ taller_c/src/statistics.c
@@ -1,37 +1,52 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
+
 #include "statistics.h"
 
 const char* const statistics_help = 
   "Stats v1.0.0 Diego Soto ...."
-
+  "Stats v1.0.0 Diego Soto ...."
+  "Stats v1.0.0 Diego Soto ...."
 ;
 
 // Registro privado
 typedef struct statistics {
   // atributos de la clase
-
-
+  bool help_asked; 
 } statistics_t;
 
 int statistics_analyze_arguments(statistics_t* statistics, int argc, char* argv[]);
 
+int statistics_print_help(void);
+
 
 void statistics_destroy(statistics_t* statistics) {
   assert(statistics);
+  free(statistics);
 }
 
 
 statistics_t* statistics_create(void) {
-  return NULL;
+
+  statistics_t* statistics = (statistics_t*) 
+    calloc(1, sizeof(statistics_t) );
+    if (statistics){
+      /* code */
+    }
+    
+  return statistics;
 }
 
 int statistics_run(statistics_t* statistics, int argc, char* argv[]) {
   int error = statistics_analyze_arguments(statistics, argc, argv);
 
-  if ((error = EXIT_SUCCESS)) {
-    /* code */
+  if ((error == EXIT_SUCCESS)) {
+    if (statistics->help_asked) {
+      return statistics_print_help();
+    }
   }
 
   return error; 
@@ -40,10 +55,24 @@ int statistics_run(statistics_t* statistics, int argc, char* argv[]) {
 int statistics_analyze_arguments(statistics_t* statistics, int argc
   , char* argv[]) {
   assert(statistics);
-  for (int index = 0; index < argc; index++)
-  {
+  // se incializa en 1 porque nos interesan los arguementos del usurio, no 
+  // el nombre del programa
+  for (int index = 1; index < argc; index++) {
+ 
     printf("argv[%d] == [%s]\n", index, argv[index]);
+
+    // Si es == 0, es que son iguales
+    if ( strcmp(argv[index], "--help") == 0) {
+      statistics->help_asked = true;
+      break;
+    }  
   }
 
+  return EXIT_SUCCESS;
+}
+
+
+int statistics_print_help(void) {
+  printf("%s", statistics_help);
   return EXIT_SUCCESS;
 }
