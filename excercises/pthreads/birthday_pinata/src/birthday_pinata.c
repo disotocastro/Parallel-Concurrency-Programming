d874dfe510e05cb36cecffe61191cd5bef35ef81
JUAN DIEGO SOTO CASTRO <juan.sotocastro@ucr.ac.cr>
2024-04-05 02:29:08 -0600

    linter

--- excercises/pthreads/birthday_pinata/src/birthday_pinata.c
+++ excercises/pthreads/birthday_pinata/src/birthday_pinata.c
@@ -16,27 +16,24 @@ typedef struct shared_data {
   uint64_t position;
   pthread_mutex_t can_access_position;
   uint64_t thread_count;
-  uint64_t pinata_count; // Contador de maximo de golpes
+  uint64_t pinata_count;  // Contador de maximo de golpes
 } shared_data_t;
 
 // thread_private_data_t
 typedef struct private_data {
   uint64_t thread_number;  // rank
   shared_data_t* shared_data;
-  bool winner; // para saber quien rompio la pinata
-  uint64_t thread_hit_counter; // Contador individual de cada hilo
-  uint64_t pinata_count_priv; // Contador de maximo de golpes
+  bool winner;  // para saber quien rompio la pinata
+  uint64_t thread_hit_counter;  // Contador individual de cada hilo
+  uint64_t pinata_count_priv;  // Contador de maximo de golpes
 } private_data_t;
 
 
 void* attack(void* data);
 int create_threads(shared_data_t* shared_data, int64_t pinata_count);
 
-// procedure main(argc, argv[])
 int main(int argc, char* argv[]) {
   int error = EXIT_SUCCESS;
-  // create thread_count as result of converting argv[1] to integer
-  // thread_count := integer(argv[1])
   uint64_t thread_count = sysconf(_SC_NPROCESSORS_ONLN);
   uint64_t pinata_count = 0;
 
@@ -44,16 +41,15 @@ int main(int argc, char* argv[]) {
     if (sscanf(argv[1], "%" SCNu64, &thread_count) == 1) {
     } else {
       fprintf(stderr, "Error: invalid thread count\n");
-      return 11;
+      return 10;
     }
 
     if (sscanf(argv[2], "%" SCNu64, &pinata_count) == 1) {
     } else {
-      fprintf(stderr, "Error: invalid thread count\n");
+      fprintf(stderr, "Error: invalid pinata count\n");
       return 11;
     }
     printf("Pinata hits: %ld\n", pinata_count);
-
   }
 
   shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
@@ -87,7 +83,7 @@ int main(int argc, char* argv[]) {
     return 12;
   }
   return error;
-}  // end procedure
+}
 
 
 int create_threads(shared_data_t* shared_data, int64_t pinata_count) {
@@ -103,10 +99,9 @@ int create_threads(shared_data_t* shared_data, int64_t pinata_count) {
       private_data[thread_number].thread_number = thread_number;
       private_data[thread_number].shared_data = shared_data;
       private_data[thread_number].thread_hit_counter = 0;
-      private_data[thread_number].winner = false; 
+      private_data[thread_number].winner = false;
       private_data[thread_number].pinata_count_priv = pinata_count;
 
-    
       // create_thread(greet, thread_number)
       error = pthread_create(&threads[thread_number], /*attr*/ NULL, attack
         , /*arg*/ &private_data[thread_number]);
@@ -156,13 +151,13 @@ void* attack(void* data) {
       // Si la piñata ya no puede recibir más golpes
       pthread_mutex_unlock(&shared_data->can_access_position);
         if (private_data->winner) {
-          printf("Thread %ld / %ld: My hit num: %ld, I broke the pinata !! \n",
-          private_data->thread_number, 
+          printf("Thread %ld/%ld: hits %ld, I broke the pinata !! \n",
+          private_data->thread_number,
           private_data[0].pinata_count_priv,
           private_data->thread_hit_counter);
         } else {
-          printf("Thread %ld / %ld: My hit num: %ld \n", 
-          private_data->thread_number, 
+          printf("Thread %ld/%ld: hits %ld,\n",
+          private_data->thread_number,
           private_data[0].pinata_count_priv,
           private_data->thread_hit_counter);
         }
