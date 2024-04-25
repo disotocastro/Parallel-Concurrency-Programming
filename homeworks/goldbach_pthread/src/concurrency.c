c9491dddd90dca8d3817508ade618c6f2f95954e
JUAN DIEGO SOTO CASTRO <juan.sotocastro@ucr.ac.cr>
2024-04-24 22:56:03 -0600

    goldbach_pthread: create threads basic logic added

--- homeworks/goldbach_pthread/src/concurrency.c
+++ homeworks/goldbach_pthread/src/concurrency.c
@@ -56,7 +56,57 @@ int golbach_concurrency(int argc, char* argv[]) {
 }
 
 int create_threads(shared_data_t* shared_data) {
+  if (sem_init(&shared_data->sem,0, 1)) {
+    // Camino feliz
 
+    pthread_t* threads = (pthread_t*) calloc
+                                 (shared_data->thread_count, sizeof(pthread_t));
+    private_data_t* private_data = (private_data_t*) calloc
+                            (shared_data->thread_count, sizeof(private_data_t));
+
+    if (threads && private_data) {
+
+      // Crear semaforos
+      for (uint64_t sem_index = 0; sem_index < shared_data->arr_input.count; 
+        sem_index++) {
+        
+        if (sem_init(&shared_data->can_print[sem_index], 0, !sem_index)) {
+          
+        } else {
+          return EXIT_FAILURE;
+        }
+      }
+
+      for (uint64_t i = 0; i < shared_data->thread_count; i++) {
+        private_data[i].thread_number = i;
+        private_data[i].shared_data = shared_data;
+
+        if (pthread_create(&threads[i], NULL, run, &private_data[i]) == 0) {
+          
+        } else {
+          shared_data->thread_count = i;
+          // implementar codigo de error
+          return EXIT_FAILURE;
+          break;
+        }
+      }
+      
+      for (uint64_t i = 0; i < shared_data->thread_count; ++i) {
+        pthread_join(threads[i], NULL);
+        sem_destroy(&shared_data->can_print[i]);
+      }
+      free(threads);
+      free(private_data);
+    } else{
+      // Implementar error aqui
+      return EXIT_FAILURE;
+    }
+                            
+
+  } else {
+    fprintf(stderr, "Error: Could not create sems memory\n");
+    return EXIT_FAILURE;
+  }
 }
 
 void* run(void* data) {
