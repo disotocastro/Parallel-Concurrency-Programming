50b460c6a4c58c1b080d824486c6b1e50f2c293f
JUAN DIEGO SOTO CASTRO <juan.sotocastro@ucr.ac.cr>
2024-04-27 20:53:31 -0600

    main changed

--- homeworks/goldbach_pthread/src/concurrency.c
+++ homeworks/goldbach_pthread/src/concurrency.c
@@ -20,41 +20,6 @@
 #include "concurrency.h"
 #include "goldbach.h"
 
-// Este codigo funciona de manera muy similar al antiguo main
-int golbach_concurrency(int argc, char* argv[]) {
-  FILE* input = stdin;
-  int error = EXIT_SUCCESS;
-  shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
-  if (shared_data) {
-    shared_data->thread_count = get_thread_count(argc, argv);
-    shared_data->this_thread_position = 0;
-    // analizar_argumentos(argc, argv);
-    // array_usuario = leer_input_usuario(input_usuario);
-    analyze_arguments(argc, argv);
-    array_init(&shared_data->arr_input);
-    read_file(input, &shared_data->arr_input);
-    int64_t largest_element = largest_element_arr(&shared_data->arr_input);
-    array_init(&shared_data->arr_prime_num);
-    trial_division(&shared_data->arr_prime_num, largest_element);
-    shared_data->can_print = (sem_t*) calloc
-                                (shared_data->arr_input.count, sizeof(sem_t));
-    if (shared_data->can_print) {
-      create_threads(shared_data);
-      free(shared_data->can_print);
-    } else {
-        fprintf(stderr, "Error: Could not allocate semaphores\n");
-        error = EXIT_FAILURE;
-    }
-    array_destroy(&shared_data->arr_prime_num);
-    array_destroy(&shared_data->arr_input);
-    free(shared_data);
-  } else {
-      fprintf(stderr, "Error: Could not create dynamic memory\n");
-      error = EXIT_FAILURE;
-  }
-  return error;
-}
-
 int create_threads(shared_data_t* shared_data) {
     sem_init(&shared_data->sem, 0, 1);
   // if (sem_init(&shared_data->sem, 0, 1)) {
